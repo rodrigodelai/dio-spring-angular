af274c98184028f09cc53be37f33510289810d62
Rodrigo Delai <rodrigo-delai@hotmail.com>
2023-10-19 15:08:57 -0300

    Start simple bank project

---
+++ simple-bank/src/test/java/me/dio/simplebank/SimpleBankApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.simplebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
