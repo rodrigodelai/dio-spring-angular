198d42acc00be716acad1ef69d2aa3bd76847e50
Rodrigo Delai <rodrigo-delai@hotmail.com>
2024-01-22 20:28:01 -0300

    Create structure and styles

---
+++ buzzfeed/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { QuizComponent } from './components/quiz/quiz.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    QuizComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
